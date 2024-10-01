b8fcfcbd5f6a9c2a88a94ca861e09a85c238c362
Arnab <arnabpramanik43@gmail.com>
2024-10-01 16:04:32 +0530

    djfj

--- src/App.js
+++ src/App.js
@@ -1,24 +1,29 @@
-import logo from './logo.svg';
-import './App.css';
+
+
+import Navbar from './components/Navbar';
+import TextInput from './components/TextInput';
+import Footer from './components/Footer';
+import About from './components/About';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>  {/* Ensure Router wraps the entire app */}
+      <>
+        <Navbar AppName="Text-C" />
+
+        <Routes>
+
+          <Route path='/' element={<TextInput title="Enter The Text" />} />
+
+          <Route path='/about' element={<About />} />
+
+        </Routes>
+        <br />
+        <br />
+        <Footer />
+      </>
+    </Router>
   );
 }
 
---
+++ src/components/Footer.js
@@ -0,0 +1,41 @@
+import React from 'react';
+// import facebook from '../../public/icons/facebook.png';
+import {Link} from 'react-router-dom'
+
+
+
+
+function Footer() {
+  return (
+
+    <div className="container">
+      <footer className="d-flex flex-wrap justify-content-between align-items-center py-3 my-4 border-top">
+        <div className="col-md-4 d-flex align-items-center">
+          <span className="mb-3 mb-md-0 text-muted">Create by Arnab, Inc</span>
+        </div>
+
+        <ul className="nav col-md-4 justify-content-end list-unstyled d-flex">
+        <li className="ms-3">
+            <Link className="text-muted" to="https://www.linkedin.com/in/arnabpramanik43/">
+              <img src="/icons/linkedin.png" alt="Linkedin" width="24" height="24" />
+            </Link>
+          </li>
+          <li className="ms-3">
+            <Link className="text-muted" to="https://github.com/Arnab141">
+              <img src="/icons/github.png" alt="github" width="24" height="24" />
+            </Link>
+          </li>
+          <li className="ms-3">
+            <Link className="text-muted" to="https://www.facebook.com/profile.php?id=100071509121247">
+              <img src="/icons/facebook.png" alt="Facebook" width="24" height="24" />
+            </Link>
+          </li>
+
+        </ul>
+
+      </footer>
+    </div>
+  );
+}
+
+export default Footer;
